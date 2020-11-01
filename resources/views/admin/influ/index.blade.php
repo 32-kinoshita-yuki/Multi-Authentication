70e3a5801b11bf3f00b02c50edae6c0a7fdd6337
”32-kinoshita-yukigit config --global user.email ”git@github.com:32-kinoshita-yuki/sample.git <inuyasya.19950406@gmail.com>
2020-11-01 16:04:52 +0000

    add

--- resources/views/admin/influ/index.blade.php
+++ resources/views/admin/influ/index.blade.php
@@ -5,6 +5,10 @@
   <div class="row">
   <div class="col-md-10 col-md-offset-2">
       <h2>インフルエンサー一覧</h2>
+     
+     
+     
+      
        @if (session('err_msg'))
       <p class="texit-danger">
          {{ session('err_msg') }}
